4a05f51fe5339b8edd8716cee26a68718bf247c2
Philip Webster <phil@remind101.com>
2017-11-21 12:18:06 -0800

    deselect

--- MultiSection/TableSection.swift
+++ MultiSection/TableSection.swift
@@ -71,6 +71,7 @@ extension TableSection: UITableViewDataSource {
 extension TableSection: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
